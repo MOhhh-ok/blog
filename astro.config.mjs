25ae8977edb903dac0edb5aa0e89e86990c0089e
MOhhh <64732359+MOhhh-ok@users.noreply.github.com>
2025-11-10 22:10:56 +0900

    -

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://mohhh-ok.github.io",
   base: "/blog",
   integrations: [mdx(), sitemap(), pagefind()],
 });
