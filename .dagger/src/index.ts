7c464cdea6bdeefddcd766e75286c11318fe8113
MOhhh <64732359+MOhhh-ok@users.noreply.github.com>
2025-11-13 22:42:34 +0900

    -

--- .dagger/src/index.ts
+++ .dagger/src/index.ts
@@ -1,59 +1,27 @@
-import { argument, Container, dag, Directory, func, object } from "@dagger.io/dagger";
+import { argument, dag, Directory, func, object } from "@dagger.io/dagger";
 
 @object()
 export class HelloDagger {
   /**
-   * Publish the application container after building and testing it on-the-fly
+   * Build Astro site and return dist directory
    */
   @func()
-  async publish(
+  async build(
     @argument({ defaultPath: "/" }) source: Directory,
-  ): Promise<string> {
-    await this.test(source);
-    return await this.build(source).publish(
-      "ttl.sh/hello-dagger-" + Math.floor(Math.random() * 10000000),
-    );
-  }
+  ): Promise<Directory> {
+    const pnpmCache = dag.cacheVolume("pnpm");
 
-  /**
-   * Build the application container
-   */
-  @func()
-  build(@argument({ defaultPath: "/" }) source: Directory): Container {
-    const build = this.buildEnv(source)
-      .withExec(["npm", "run", "build"])
-      .directory("./dist");
-    return dag
-      .container()
-      .from("nginx:1.25-alpine")
-      .withDirectory("/usr/share/nginx/html", build)
-      .withExposedPort(80);
-  }
-
-  /**
-   * Return the result of running unit tests
-   */
-  @func()
-  async test(
-    @argument({ defaultPath: "/" }) source: Directory,
-  ): Promise<string> {
-    return this.buildEnv(source)
-      .withExec(["npm", "run", "test:unit", "run"])
-      .stdout();
-  }
-
-  /**
-   * Build a ready-to-use development environment
-   */
-  @func()
-  buildEnv(@argument({ defaultPath: "/" }) source: Directory): Container {
-    const nodeCache = dag.cacheVolume("node");
     return dag
       .container()
       .from("node:21-slim")
-      .withDirectory("/src", source)
-      .withMountedCache("/root/.npm", nodeCache)
-      .withWorkdir("/src")
-      .withExec(["npm", "install"]);
+      .withExec(["corepack", "enable"])
+      .withExec(["corepack", "prepare", "pnpm@latest", "--activate"])
+      .withDirectory("/app", source)
+      .withMountedCache("/root/.local/share/pnpm/store", pnpmCache)
+      .withWorkdir("/app")
+      .withEnvVariable("CI", "true")
+      .withExec(["pnpm", "install"])
+      .withExec(["pnpm", "run", "build"])
+      .directory("/app/dist");
   }
 }
