5bb1cbfa805506c029fec40122c2d926a0527dbd
MOhhh <64732359+MOhhh-ok@users.noreply.github.com>
2025-12-04 09:35:22 +0900

    -

--- src/pages/sitemap.txt.ts
+++ src/pages/sitemap.txt.ts
@@ -5,7 +5,7 @@ import { SITE_DESCRIPTION, SITE_TITLE } from "../consts";
 export async function GET(context: any) {
   const posts = await getCollection("blog");
   const res = new Response();
-  return new Response(posts.map((post) => `${context.site}blog/${post.id}`).join("\n"));
+  return new Response(posts.map((post) => `${context.site}blog/${encodeURIComponent(post.id)}`).join("\n"));
   // ({
   //     ...post.data,
   //     pubDate: post.data.pubDate,
